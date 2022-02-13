a165ffd016618e59c0ab19197a85b0d3adab0dfd
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-13 15:13:14 +0000

    Add centering to numbers

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,20 @@
 // global variables
 let temp = document.getElementsByClassName('game-grid-cells');
-
+let counter = 1;
+let emptyArray = [];
 
 
 function startGame(){
     
     for(let i = 0; i < temp.length; i++){
-        temp[i].innerHTML = `<span> ${i+1}</span>`;
+        emptyArray.push(counter);
+        temp[i].innerHTML = `<div class="numbers"> ${counter++}</div>`;
+        if(counter > 9){
+            counter = 1;
+        }
+        
     }
+    alert(emptyArray);
 }
 
 startGame();
