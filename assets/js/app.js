cb5dbe1675baf2a13188cdfa87dc5b2a04bcf0f8
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-13 18:14:00 +0000

    Test random number positioning

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,32 @@
 // global variables
 let emptyGridCells = document.getElementsByClassName('game-grid-cells');
-let vertical = 9;
-let width = 9;
+
+const vertically = 9;
+const horizontally = 9;
+
 let filledGridCells = []; 
 let possibleChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let localZone = []; // 9 positions
 let conflictZone = []; // 19 positions
 let emptyArray = [];
 
+let firstValue = Math.floor(Math.random() * 9);
+let firstPosition = Math.floor(Math.random() * 81);
+
 // function to start game
 function startGame(){
-    let firstValue = Math.floor(Math.random() * 9);
-    let firstPosition = Math.floor(Math.random() * 81);
-    if(firstValue === 0 || firstPosition === 0){
+    firstValue = Math.floor(Math.random() * 9);
+    firstPosition = Math.floor(Math.random() * 81);
+
+    if(firstValue === 0){
         firstValue++;
+    } else if(firstPosition === 0){
         firstPosition++;
     }
 
-    
+    emptyGridCells[firstPosition].innerHTML = `<div class="numbers"> ${firstValue}</div>`;
+
+
 
 
 
