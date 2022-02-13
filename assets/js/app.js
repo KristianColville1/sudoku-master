208bdcad575d0850215437f6eaa0a39b74240b1b
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-13 18:03:18 +0000

    Add first position and first value random values

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 // global variables
 let emptyGridCells = document.getElementsByClassName('game-grid-cells');
+let vertical = 9;
+let width = 9;
 let filledGridCells = []; 
 let possibleChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let localZone = []; // 9 positions
@@ -8,22 +10,22 @@ let emptyArray = [];
 
 // function to start game
 function startGame(){
-
-
-
-
-
-
-
-
-
+    let firstValue = Math.floor(Math.random() * 9);
+    let firstPosition = Math.floor(Math.random() * 81);
+    if(firstValue === 0 || firstPosition === 0){
+        firstValue++;
+        firstPosition++;
+    }
 
     
+
+
+
     // let counter = 1;
     // enters the numbers 1 - 9 every 9 numbers
-    // for(let i = 0; i < temp.length; i++){
+    // for(let i = 0; i < emptyGridCells.length; i++){
     //     emptyArray.push(counter);
-    //     temp[i].innerHTML = `<div class="numbers"> ${counter++}</div>`;
+    //     emptyGridCells[i].innerHTML = `<div class="numbers"> ${firstValue}</div>`;
     //     if(counter > 9){
     //         counter = 1;
     //     }
