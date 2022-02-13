02f24b20e3dbb16bc2ba367e1aedd6f5d7502765
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-13 17:10:20 +0000

    Add new variables to hold arrays and begin creating game boards

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,37 @@
 // global variables
-let temp = document.getElementsByClassName('game-grid-cells');
-let counter = 1;
+let emptyGridCells = document.getElementsByClassName('game-grid-cells');
+let filledGridCells = []; 
+let possibleChoices = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let localZone = []; // 9 positions
+let conflictZone = []; // 19 positions
 let emptyArray = [];
 
-
+// function to start game
 function startGame(){
+
+
+
+
+
+
+
+
+
+
     
-    for(let i = 0; i < temp.length; i++){
-        emptyArray.push(counter);
-        temp[i].innerHTML = `<div class="numbers"> ${counter++}</div>`;
-        if(counter > 9){
-            counter = 1;
-        }
+    // let counter = 1;
+    // enters the numbers 1 - 9 every 9 numbers
+    // for(let i = 0; i < temp.length; i++){
+    //     emptyArray.push(counter);
+    //     temp[i].innerHTML = `<div class="numbers"> ${counter++}</div>`;
+    //     if(counter > 9){
+    //         counter = 1;
+    //     }
         
-    }
-    alert(emptyArray);
+    // }
+    
+    // alert(emptyArray);
+
 }
 
-startGame();
+startGame(); // calls the function and runs the game
