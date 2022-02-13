db948a8f5875f3d0f5b6bd8b4bcdafa167c64ffd
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-13 18:33:01 +0000

    Clean code and create functions for random position and random number

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,33 +10,15 @@ let localZone = []; // 9 positions
 let conflictZone = []; // 19 positions
 let emptyArray = [];
 
-let firstValue = Math.floor(Math.random() * 9);
-let firstPosition = Math.floor(Math.random() * 81);
-
 // function to start game
 function startGame(){
-    firstValue = Math.floor(Math.random() * 9);
-    firstPosition = Math.floor(Math.random() * 81);
-
-    if(firstValue === 0){
-        firstValue++;
-    } else if(firstPosition === 0){
-        firstPosition++;
-    }
-
-    for()
-
-    emptyGridCells[firstPosition].innerHTML = `<div class="numbers"> ${firstValue}</div>`;
-
-
-
-
+    emptyGridCells[newPosition].innerHTML = `<div class="numbers"> ${newValue}</div>`;
 
     // let counter = 1;
     // enters the numbers 1 - 9 every 9 numbers
     // for(let i = 0; i < emptyGridCells.length; i++){
     //     emptyArray.push(counter);
-    //     emptyGridCells[i].innerHTML = `<div class="numbers"> ${firstValue}</div>`;
+    //     emptyGridCells[i].innerHTML = `<div class="numbers"> ${newValue}</div>`;
     //     if(counter > 9){
     //         counter = 1;
     //     }
@@ -48,8 +30,19 @@ function startGame(){
 }
 
 // Use functions to call when new random numbers needed
-function firstValue(){
+function newValue(){
     let num = Math.floor(Math.random() * 9);
+    if(num === 0){
+        num++;
+    }
+    return num;
+}
+
+function newPosition(){
+    let num = Math.floor(Math.random() * 81);
+    if(num === 0){
+        num++;
+    }
     return num;
 }
 
