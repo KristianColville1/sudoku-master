9511e0644fac5dc512316f23b5762de6efe8e05a
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-14 11:17:57 +0000

    Test traversing through grid and accessing vertical positions and horizontal positions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 // global variables
-let emptyGridCells = document.getElementsByClassName('game-grid-cells');
+let gameGridCells = document.getElementsByClassName('game-grid-cells');
 
 const vertically = 9;
 const horizontally = 9;
@@ -47,55 +47,25 @@ let largeGridCells = [ [
     79, 80, 81
 ]];
 
+
+
+
 // emptyCells[i].innerHTML = `<span class="numbers"> ${randNum()}</span>`;
 
 // function to start game
 function startGame(){
 
-    // I want to test iterating through the large cell array and adding the values to the empty array to check it works
-
-    // emptyGridCells[largeGridCells[0][0]-1].innerHTML = `<span class="numbers"> ${randNum()}</span>`;
-
-    // iterating through the array is successful.
-    // for(let outside = 0; outside < possibleChoices.length; outside++){
-    //     for(let inside = 0; inside < possibleChoices.length; inside++){
-    //         emptyGridCells[largeGridCells[outside][inside]-1].innerHTML = `<span class="numbers"> ${randNum()}</span>`;
-    //     }
-    // }
-
-    // test iterating through 1 grid and placing values
-    // for(let i = 0; i < possibleChoices.length; i++){
-    //     emptyGridCells[largeGridCells[0][i]- 1].innerHTML = `<span class="numbers"> ${randNum()}</span>`;
-    // }
-    // testing iterating through one grid at a time successful
-
-
-
-    // Now we want to place a random value in a random grid and random position
-    // emptyGridCells[largeGridCells[randNum()][randNum()]- 1].innerHTML = `<span class="numbers"> ${randNum()}</span>`;
-    // for(let i = 0; i < possibleChoices.length; i++){
-    //     emptyGridCells[largeGridCells[9 - 1][i]- 1].innerHTML = `<span class="numbers"> ${randNum()}</span>`;
-    // }
-
-    // adding 81 positions to the empty array from 0 - 81
-    for(let i = 0; i < emptyGridCells.length; i++){
-        emptyArray.push('null');
-    }
-
-    // solve the board and save it the the empty array and the final array.
-
-
-
-
+    
 }
 
 // Use functions to call when new random numbers needed
-function randNum(){
-    let num = Math.floor(Math.random() * 9);
+function randNum(n){
+    let num = Math.floor(Math.random() * n);
     if(num === 0){
         num++;
     }
     return num;
 }
+
 
 startGame(); // calls the function and runs the game
