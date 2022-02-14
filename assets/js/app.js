ee0d9c2a149221d42e0be55987f02a2c5658067c
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-14 17:40:41 +0000

    Add new array for horizontal and verical for testing

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,16 +14,28 @@ let playerIndex = 0;
 let playerPos = [41]; // places player in the center of the board
 
 
-let largeGridCells = [
-    [0,1,2,3,4,5,6,7,8],
-    [9,10,11,12,13,14,15,16,17],
-    [18,19,20,21,22,23,24,25,26],
-    [27,28,29,30,31,32,33,34,35],
-    [36,37,38,39,40,41,42,43,44],
-    [45,46,47,48,49,50,51,52,53],
-    [54,55,56,57,58,59,60,61,62],
-    [63,64,65,66,67,68,69,70,71],
-    [72,73,74,75,76,77,78,79,80]
+let horizontalGrid = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    [9, 10, 11, 12, 13, 14, 15, 16, 17],
+    [18, 19, 20, 21, 22, 23, 24, 25, 26],
+    [27, 28, 29, 30, 31, 32, 33, 34, 35],
+    [36, 37, 38, 39, 40, 41, 42, 43, 44],
+    [45, 46, 47, 48, 49, 50, 51, 52, 53],
+    [54, 55, 56, 57, 58, 59, 60, 61, 62],
+    [63, 64, 65, 66, 67, 68, 69, 70, 71],
+    [72, 73, 74, 75, 76, 77, 78, 79, 80]
+];
+
+let verticalGrid = [
+    [0, 9, 18, 27, 36, 45, 54, 63, 72],
+    [1, 10, 19, 28, 37, 46, 55, 64, 73],
+    [2, 11, 20, 29, 38, 47, 56, 65, 74],
+    [3, 12, 21, 30, 39, 48, 57, 66, 75],
+    [4, 13, 22, 31, 40, 49, 58, 67, 76],
+    [5, 14, 23, 32, 41, 50, 59, 68, 77],
+    [6, 15, 24, 33, 42, 51, 60, 69, 78],
+    [7, 16, 25, 34, 43, 52, 61, 70, 79],
+    [8, 17, 26, 35, 44, 53, 62, 71, 80]
 ];
 
 // emptyCells[i].innerHTML = `<span class="numbers"> ${randNum()}</span>`;
@@ -61,16 +73,15 @@ function createBoard(){
 
 }
 
+// function to search the main array to locate the player class and highlight the array with class shader horizontally
 function checkGridArray(){
-    // alert(largeGridCells[0]);
-    for(let outerArray = 0; outerArray < largeGridCells.length; outerArray++){
-        var innerArraylength = largeGridCells[outerArray].length;
+    for(let outerArray = 0; outerArray < horizontalGrid.length; outerArray++){
+        var innerArraylength = horizontalGrid[outerArray].length;
         for(let innerArray = 0; innerArray < innerArraylength; innerArray++){
-            if(gameGridCells[largeGridCells[outerArray][innerArray]].classList.contains('player')){
-                playerIndex = largeGridCells[outerArray][innerArray];
-                largeGridCells[outerArray].forEach(index => gameGridCells[index].classList.add('shaded'));
-                largeGridCells[outerArray].forEach(index => shadedPos.push(index));
-                // largeGridCells[outerArray].forEach(index => gameGridCells[index - 1].innerHTML = `<span class="numbers"> ${randNum(9 - innerArray)}</span>`);
+            if(gameGridCells[horizontalGrid[outerArray][innerArray]].classList.contains('player')){
+                playerIndex = horizontalGrid[outerArray][innerArray];
+                horizontalGrid[outerArray].forEach(index => gameGridCells[index].classList.add('shaded'));
+                horizontalGrid[outerArray].forEach(index => shadedPos.push(index));
             }
         }
     }
