ac2ef51abb12aa942a554b03a9845d56924c72b0
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-18 11:41:49 +0000

    Add solved board

---
+++ assets/js/sudoku-board.js
@@ -0,0 +1,23 @@
+// Approach four:
+
+// Sudoku Rules:
+// the grid contains 81 cells made of 9 x 9 cells
+// horizontal cell numbers need to include 1 to 9 
+// vertical cell numbers need to include 1 to 9
+// each 3 x 3 must also contain the numbers 1 to 9
+
+// 17 valid sudoku numbers are needed to generate a single unique solution.
+
+// solved sudoku board
+let solvedBoard =
+[
+    1, 2, 3, 4, 5, 6, 7, 8, 9,
+    2, 3, 4, 5, 6, 7, 8, 9, 1,
+    3, 4, 5, 6, 7, 8, 9, 1, 2,
+    4, 5, 6, 7, 8, 9, 1, 2, 3,
+    5, 6, 7, 8, 9, 1, 2, 3, 4,
+    6, 7, 8, 9, 1, 2, 3, 4, 5,
+    7, 8, 9, 1, 2, 3, 4, 5, 6,
+    8, 9, 1, 2, 3, 4, 5, 6, 7,
+    9, 1, 2, 3, 4, 5, 6, 7, 8,
+]
