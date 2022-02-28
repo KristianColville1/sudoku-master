215b00c1e096263d959c662a1042af0f0e28618c
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-28 16:10:38 +0000

    Test google sign in

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,14 @@
-function onSignIn(googleUser){
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
     var profile = googleUser.getBasicProfile();
-    $('#name').text(profile.getName());
-    $('#email').text(profile.getEmail());
-    $('#image').attr('src', profile.getImageUrl());
-    $('.data').css('display', 'block');
-    $('.g-signin2').css('display', 'none');
-}
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
 
-function signOut(){
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        alert('You are now signed out');
-        $('.g-signin2').css('display', 'block');
-    });
-}
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+  }
