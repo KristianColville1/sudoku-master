46e637326320f26f81a0afe0acef5b3684f9c8c6
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-03-06 13:40:38 +0000

    Remove hidden when msg sends

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -5,6 +5,7 @@ const sendEmailBtn = document.getElementById('contact-btn');
 document.getElementById('contact-form').addEventListener('submit', function (event){
     event.preventDefault();
     emailjs.init('JBCmNL6wutFtO1NM7');
+    sendEmailBtn.value = 'Sending...';
 
     emailjs.sendForm('service_5c7vg0b', 'template_dz7rfjt', this)
     .then(function() {
@@ -19,5 +20,8 @@ document.getElementById('contact-form').addEventListener('submit', function (eve
 // hides contact form and displays thank you message on success
 
 function displayThanks(){
-    
+    let form = document.getElementById('contact-form');
+    let thanks = document.getElementById('thank-you-msg');
+    form.classList.add('hidden');
+    thanks.classList.remove('hidden');
 }
